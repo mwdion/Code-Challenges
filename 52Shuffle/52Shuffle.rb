09e69df5670fb946b5e4e47fadaac2d3523826bb
Michael Dion <mwdion@gmail.com>
2014-11-03 12:55:46 -0500

    working on unit test

--- 52Shuffle/52Shuffle.rb
+++ 52Shuffle/52Shuffle.rb
@@ -1,9 +1,18 @@
 class Shuffle
   def initialize(cards)
-    @cards = (1..52).to_a
+    @cards = Array.new
   end
   def shuffle
     cards.sort_by{rand}
   end
 end
+
+# class Shuffle
+#   def initialize(cards)
+#     @cards = (1..52).to_a
+#   end
+#   def shuffle
+#     cards.sort_by{rand}
+#   end
+# end
 
--- 52Shuffle/spec.rb
+++ 52Shuffle/spec.rb
@@ -5,23 +5,8 @@ class ShuffleTest < MiniTest::Unit::TestCase
   describe "Shuffle", "A simple shuffle test" do
 
   it "randomly sorts the cards" do
-    shuffle = Shuffle.new(cards) 
-    shuffle.cards.wont_equal cards
+    cards = Shuffle.new((1..52).to_a)
+    cards.shuffle.wont_equal (cards)
   end
 end
 end
-# michael = person.new
-# person.blank should equal 
-
-# it "should shuffle the array of cards" do
-#     expect(cards.sort_by.rand).not_to eq(cards)
-#  end
-
-# describe "Person", "A simple person example" do
-
-#   it "has a full name" do
-#     person = Person.new("Yukihiro", "Matsumoto")
-#     person.full_name.must_equal "Yukihiro Matsumoto"
-#   end
-
-# end
